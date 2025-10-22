4b17ff74691a5f96c9d74753ff29496ad1cd1214
ujangbedog <domba88galing@gmail.com>
2025-10-22 14:12:59 +0700

    feat: add new product pages and content structure

---
+++ pages/produk/hoodie/index.tsx
@@ -0,0 +1,104 @@
+import { Suspense, useState } from "react";
+import { NextSeo } from "next-seo";
+import Image from "next/image";
+import {
+  Box,
+  Container,
+  Text,
+  Grid,
+  GridItem,
+  Filter,
+  useDisclosure,
+  AspectRatio,
+} from "@/lib/ui";
+import { HOODIE, HOODIE_TYPES } from "@/content/produk/hoodie";
+import { useRouter } from "next/router";
+import { kebabCase } from "lodash-es";
+import { useIsClient } from "usehooks-ts";
+import { useFilter } from "@/hooks/useFilter";
+import ProductCard, {
+  ProductCardSkeleton,
+} from "@/components/Produk/ProductCard";
+import ProductModal from "@/components/Produk/ProductModal";
+
+interface FilterOption {
+  label: string;
+  value: string;
+}
+
+const filterOptions: FilterOption[] = [
+  { label: "Semua", value: "all" },
+  ...HOODIE_TYPES.map((type) => ({ label: type, value: kebabCase(type) })),
+];
+
+function Cards() {
+  const { defaultOption, filteredItems, handleFilterChange } = useFilter(
+    filterOptions,
+    HOODIE
+  );
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Filter
+        options={filterOptions}
+        selectedOption={defaultOption || filterOptions[0]}
+        onChange={handleFilterChange}
+        mb={{ base: 6, md: 8 }}
+      />
+      <Grid
+        templateColumns={{
+          base: "100%",
+          lg: "repeat(2, 1fr)",
+          xl: "repeat(3, 1fr)",
+        }}
+        gap={6}
+      >
+        {filteredItems.map((item) => (
+          <GridItem key={item.name} display="flex">
+            <ProductCard
+              product={item}
+              onImageClick={(image) => {
+                setSelectedImage(image);
+                onOpen();
+              }}
+            />
+          </GridItem>
+        ))}
+      </Grid>
+      <ProductModal
+        isOpen={isOpen}
+        onClose={onClose}
+        selectedImage={selectedImage}
+      />
+    </>
+  );
+}
+
+export default function HoodiePage() {
+  const router = useRouter();
+  const isClient = useIsClient();
+
+  return (
+    <>
+      <NextSeo
+        title="Sleep Walker Produk | Hoodie"
+        description="Temukan koleksi hoodie Sleep Walker dengan berbagai pilihan."
+      />
+      <Box as="section" pt={{ base: 8, md: 10 }} pb={{ base: 6, md: 8 }}>
+        <Container maxW="container.2xl" px={{ base: 5, lg: 10, xl: 16 }}>
+          <Box mb={{ base: 6, md: 8 }}>
+            <Text textStyle="h3" mb={2} textAlign="center">
+              Hoodie
+            </Text>
+          </Box>
+
+          <Suspense fallback={<ProductCardSkeleton />}>
+            {isClient && router.isReady && <Cards />}
+          </Suspense>
+        </Container>
+      </Box>
+    </>
+  );
+}
